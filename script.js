f197b0d35cccee861c48fbfa2a535fdb948f90f0
Lewis Wake <wakelewis74@gmail.com>
2026-07-14 13:52:46 +0100

    Update scanned pages, issues, and last updated date
    Updated the number of manually scanned pages and issues, along with the last updated date.

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const totalPagesInRun = 7512;
   // MANUALLY UPDATE THIS VALUE AS PAGES ARE SCANNED
-  const manualScannedPages = 3788;
+  const manualScannedPages = 3920;
 
   const pagesFill = document.getElementById("pages-progress-fill");
   const pagesText = document.getElementById("pages-progress-text");
 
   // MANUALLY UPDATE THESE VALUES WHEN NEW RAW 600 DPI SCANS ARE COMPLETED
-  const manualScannedIssues = 107;
-  const lastUpdatedDate = "July 13, 2026";
+  const manualScannedIssues = 111;
+  const lastUpdatedDate = "July 14, 2026";
 
   // State for filtering
   let currentSearch = "";
