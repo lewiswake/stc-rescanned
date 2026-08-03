76b0c33a425e290f263498cfcc24974b789ba384
Lewis Wake <wakelewis74@gmail.com>
2026-08-03 12:38:37 +0100

    Update scanned pages and issues count

--- script.js
+++ script.js
@@ -20,14 +20,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   const totalPagesInRun = 7512;
   // MANUALLY UPDATE THIS VALUE AS PAGES ARE SCANNED
-  const manualScannedPages = 4696;
+  const manualScannedPages = 4728;
 
   const pagesFill = document.getElementById("pages-progress-fill");
   const pagesText = document.getElementById("pages-progress-text");
 
   // MANUALLY UPDATE THESE VALUES WHEN NEW RAW 600 DPI SCANS ARE COMPLETED
-  const manualScannedIssues = 135;
-  const lastUpdatedDate = "August 2, 2026";
+  const manualScannedIssues = 136;
+  const lastUpdatedDate = "August 3, 2026";
 
   // State for filtering
   let currentSearch = "";
